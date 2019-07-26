20011e89a3b0c86c8489b09a7192d58e984362ce
chrisribia <31062468+chrisribia@users.noreply.github.com>
2019-07-26 12:17:57 +0300

    add time complexity

--- stactadt.py
+++ stactadt.py
@@ -5,12 +5,16 @@ class Stack:
         self.stack_adt = []
 
     def length(self):
-        """ getting the length of stack"""
+        """ getting the length of stack
+        time = O(n)
+        """
 
         return len(self.stack_adt)
 
     def is_empty(self):
-        """check if stack is empty"""
+        """check if stack is empty
+        time = O(1)
+        """
 
         if self.length == 0:
             return True
@@ -18,12 +22,17 @@ class Stack:
             return False
 
     def add_item(self,item):
-        """add item to a stack"""
+        """add item to a stack
+          Constant time: O(1)
+        """
         return self.stack_adt.append(item)
 
     
     def remove_item(self):
-        """remove last element from stack"""
+        """remove last element from stack
+         Time:
+            constant time: O(1)  
+        """
 
         if self.is_empty:
             raise Exception("stack is Empty")
